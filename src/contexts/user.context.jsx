b781747f04ed4d14b1936eba81bd292e73c9cb19
david.cao <david539300@hotmail.com>
2025-07-31 21:47:04 +0800

    saving

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,6 +1,5 @@
 import { createContext, useEffect, useReducer } from 'react';
 
-import { createAction } from '../utils/reducer/reducer.utils';
 
 import {
   onAuthStateChangedListener,
@@ -12,30 +11,34 @@ export const UserContext = createContext({
   currentUser: null,
 });
 
-export const USER_ACTION_TYPES = {
-  SET_CURRENT_USER: 'SET_CURRENT_USER',
-};
-
-const INITIAL_STATE = {
-  currentUser: null,
-};
-
+export const USER_ACTION_TYPES ={
+  SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+const INTITAL_STATE = {
+  currentUser:null
+}
 const userReducer = (state, action) => {
-  const { type, payload } = action;
-
-  switch (type) {
+ const {type, payload} = action;
+ 
+  switch(type){
     case USER_ACTION_TYPES.SET_CURRENT_USER:
-      return { ...state, currentUser: payload };
+      return {
+        ...state, // give me the value of the pervious state object aswell for the updated ones 
+        currentUser: payload
+      }
     default:
-      throw new Error(`Unhandled type ${type} in userReducer`);
+      throw new Error(`Unhandled type ${type}  in userReducer`)
   }
 };
 
-export const UserProvider = ({ children }) => {
-  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
 
-  const setCurrentUser = (user) =>
-    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
+
+export const UserProvider = ({ children }) => {
+  const [{currrentUser}, dispatch] = useReducer(userReducer, INTITAL_STATE);
+
+ const setCurrentUser = (user) =>
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, currentUser: user });
+
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
