9ec277464e98eeac4a9934acd2a1a894e9d4b809
david.cao <david539300@hotmail.com>
2025-08-02 20:14:54 +0800

    added userReducer to migrate our user-context and cart-context from useState and useEffect

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,5 +1,7 @@
 import { createContext, useEffect, useReducer } from 'react';
 
+import { createAction } from '../utils/reducer/reducer.utils';
+
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
@@ -19,6 +21,8 @@ const INITIAL_STATE = {
 };
   
 const userReducer = (state, action) => {
+  console.log('disptach')
+  console.log(action)
   const { type, payload } = action;
 
   switch (type) {
@@ -31,9 +35,9 @@ const userReducer = (state, action) => {
 
 export const UserProvider = ({ children }) => {
   const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
-
+ console.log(currentUser)
   const setCurrentUser = (user) =>
-    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, currentUser: user });
+    dispatch( createAction( USER_ACTION_TYPES.SET_CURRENT_USER, user));
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
---
+++ src/utils/reducer/reducer.utils.js
@@ -0,0 +1,2 @@
+export const createAction = (type, payload) => ({type, payload});
+// omits the need to write type and payload when setting an action for dispatch 
